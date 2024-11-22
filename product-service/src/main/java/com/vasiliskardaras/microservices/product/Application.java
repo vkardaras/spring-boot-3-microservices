5c88cadf86c01766f209f0b71fd6c54172851822
Vasilis Kardaras <vasiliskardaras@gmail.com>
2024-11-22 15:11:44 +0200

    add db connection

--- product-service/src/main/java/com/vasiliskardaras/microservices/product/Application.java
+++ product-service/src/main/java/com/vasiliskardaras/microservices/product/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProductServiceApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductServiceApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- product-service/src/test/java/com/vasiliskardaras/microservices/product/ApplicationTests.java
+++ product-service/src/test/java/com/vasiliskardaras/microservices/product/ApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
-class ProductServiceApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {

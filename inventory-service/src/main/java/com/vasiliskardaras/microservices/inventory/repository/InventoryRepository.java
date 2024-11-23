8fe0b2add45363262ce181bda538c0e5b28dac73
Vasilis Kardaras <vasiliskardaras@gmail.com>
2024-11-23 12:40:10 +0200

    add inventory service

---
+++ inventory-service/src/main/java/com/vasiliskardaras/microservices/inventory/repository/InventoryRepository.java
@@ -0,0 +1,8 @@
+package com.vasiliskardaras.microservices.inventory.repository;
+
+import com.vasiliskardaras.microservices.inventory.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, Integer quantity);
+}

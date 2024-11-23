665b27d0f1439a4339ae220f040f582579853159
Vasilis Kardaras <vasiliskardaras@gmail.com>
2024-11-23 21:36:50 +0200

    add wiremock to order test

--- order-service/src/main/java/com/vasiliskardaras/microservices/order/config/RestClientConfig.java
+++ order-service/src/main/java/com/vasiliskardaras/microservices/order/config/RestClientConfig.java
@@ -1,6 +1,7 @@
 package com.vasiliskardaras.microservices.order.config;
 
 import com.vasiliskardaras.microservices.order.client.InventoryClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
@@ -10,9 +11,12 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 @Component
 public class RestClientConfig {
 
+    @Value("${inventory.url}")
+    String inventoryUrl;
+
     @Bean
     InventoryClient inventoryClient() {
-        RestClient client = RestClient.create("http://localhost:8082");
+        RestClient client = RestClient.create(inventoryUrl);
         HttpServiceProxyFactory factory = HttpServiceProxyFactory
                 .builderFor(RestClientAdapter.create(client))
                 .build();

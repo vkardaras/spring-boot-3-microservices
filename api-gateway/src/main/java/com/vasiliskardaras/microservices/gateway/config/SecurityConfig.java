c68e449ef673a2e8b09c9f4fa85bb555b2a5a73b
Vasilis Kardaras <vasiliskardaras@gmail.com>
2024-11-24 20:59:11 +0200

    add documentation with swagger

--- api-gateway/src/main/java/com/vasiliskardaras/microservices/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/vasiliskardaras/microservices/gateway/config/SecurityConfig.java
@@ -9,10 +9,14 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+    private final String[] freeResourceUrls = {"/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**",
+            "/swagger-resources/**", "/api-docs/**", "/aggregate/**"};
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests(authorize -> authorize.anyRequest()
-                .authenticated())
+        return http.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(freeResourceUrls).permitAll()
+                        .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
     }
